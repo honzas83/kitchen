8375ad80ce8f5d59671480441096f11a5cb2ed39
honzas <honzas@ntis.zcu.cz>
2015-07-22 14:10:42 +0200

    sklearn dependency

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Lasagne', 'theano', 'sklearn', 'numpy'],
+    install_requires=['Lasagne', 'theano', 'scikit-learn', 'numpy'],
     
     dependency_links=[
         'git+https://github.com/Lasagne/Lasagne.git#egg=Lasagne-0.1',
