a7a6b1f7c97947a4370da981182dd82de610caa4
honzas <honzas@ntis.zcu.cz>
2015-07-22 13:58:25 +0200

    Changed lasagne --> Lasagne

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['lasagne', 'theano', 'sklearn', 'numpy'],
+    install_requires=['Lasagne', 'theano', 'sklearn', 'numpy'],
     
 
     # To provide executable scripts, use entry points in preference to the
